f8130123113784359d437b1ac02324ba63ed2e8f
Cauê Foyth <foythcaue@gmail.com>
2025-04-02 19:10:31 -0300

    Atividade 9: Introdução ao regex

--- Frontend Legado/js/site.js
+++ Frontend Legado/js/site.js
@@ -30,6 +30,22 @@ function validaFaleConosco(){
         document.frmfaleconosco.txacomentario.focus();
         return false;
     }
+
+    var nome = document.frmfaleconosco.txtnome.value;
+    var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([ ]{1}[A-zÀ-ü]{2,})+$");
+    if(!expRegNome.test(nome)){
+        alert("Nome inválido!");
+        document.frmfaleconosco.txtnome.focus();
+        return false;
+    }
+
+    var fone = document.frmfaleconosco.txtfone.value;
+    var expRegFone = new RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$");
+    if(!expRegFone.test(fone)){
+        alert("Telefone inválido!");
+        document.frmfaleconosco.txtfone.focus();
+        return false;
+    }
     return true;
 }
 
