5022a21f0e9beaf64a773d37231e5d9efa60ac55
Cauê Foyth <foythcaue@gmail.com>
2025-05-22 20:06:11 -0300

    OT3

---
+++ 08 - JavaScript Atualização/OT3/script005.js
@@ -0,0 +1,6 @@
+for (i = 0; i < 20; i++){
+    if (i === 13){
+        continue;
+    }
+    console.log(i);
+}
---
+++ 08 - JavaScript Atualização/OT3/script006.js
@@ -0,0 +1,5 @@
+const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
+
+for (name of names) {
+  console.log(name);
+}
