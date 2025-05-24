81f83a1f051742944891c473f0d53c1cc94f4a59
Cauê Foyth <foythcaue@gmail.com>
2025-05-24 11:00:13 -0300

    0T8 - DESAFIO

---
+++ 08 - JavaScript Atualização/OT8 - DESAFIO/script.js
@@ -0,0 +1,25 @@
+let myvar;
+
+myvar = 10;
+
+let soma = 15 + 8;
+
+soma += 1;
+
+soma *= 3;
+
+console.log("Soma: ", soma);
+
+let unisenai = true;
+
+let comida = ["arroz", "feijão", "ovo"];
+
+console.log(comida[1]);
+
+console.log("Soma é igual a myvar?", soma === myvar);
+
+console.log("myvar é menor ou igual a soma?", myvar <= soma);
+
+let divisao = (a, b) => a / b;
+
+console.log("Divisão: ", divisao(10, 2));
