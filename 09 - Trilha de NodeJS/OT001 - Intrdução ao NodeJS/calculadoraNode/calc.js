8977bd63429cb5c023ec1b42864dd31275df748a
Cauê Foyth <foythcaue@gmail.com>
2025-05-29 14:11:19 -0300

    0T001 - INTRODUÇÃO AO NODEJS

---
+++ 09 - Trilha de NodeJS/OT001 - Intrdução ao NodeJS/calculadoraNode/calc.js
@@ -0,0 +1,9 @@
+let SomaFun = require("./somar");
+let SubFun = require("./subtracao");
+let MultFun = require("./multiplicacao");
+let DivFun = require("./divisao");
+
+console.log(`O valor da soma é: ${SomaFun(1, 2)}`);
+console.log(`O valor da divisão é: ${DivFun(5, 1)}`);
+console.log(`O valor da multiplicação é: ${MultFun(5, 4)}`);
+console.log(`O valor da subtração é: ${SubFun(10, 2)}`);
